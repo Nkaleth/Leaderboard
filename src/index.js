81fb3c2b46e48ccd024d093b49b8d8afe97d0fcb
Nkaleth <nseguralu@gmail.com>
2023-02-01 18:00:32 -0500

    Add web page layout according requirements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const addScore = document.querySelector('.addButton');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+addScore.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
